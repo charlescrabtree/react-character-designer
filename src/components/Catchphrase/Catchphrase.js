d966f7e9083300360acad0eeaf7d8749f1961ceb
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-16 14:45:56 -0700

    added folders and pages as part of initial planning

---
+++ src/components/Catchphrase/Catchphrase.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function Catchphrase() {
+  return (
+    <div>Catchphrase</div>
+  );
+}
---
+++ src/components/Character/Character.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default function character() {
+  return (
+    <div>character</div>
+  );
+}
