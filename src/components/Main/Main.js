29cc6b8db4b47c26db6f4f9f2e9f4018b3a98e99
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-28 19:14:24 -0700

    catchphrase works

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -3,6 +3,7 @@ import '../Main/Main.css';
 
 import Controls from '../Controls/Controls';
 import Catchphrase from '../Catchphrase/Catchphrase';
+import Display from '../Display/Display';
 import Character from '../Character/Character';
 
 import background from '../../background/skyrimBackground.jpg';
@@ -10,9 +11,12 @@ import background from '../../background/skyrimBackground.jpg';
 
 export default function Main() {
   const [head, setHead] = useState('DragonplateHelmet');
+  const [headCount, setHeadCount] = useState(0);
   const [body, setBody] = useState('DragonplateArmor');
+  const [bodyCount, setBodyCount] = useState(0);
   const [bottom, setBottom] = useState('DragonplateBoots');
-  const [catchphrase, setCatchphrases] = useState([]);
+  const [bottomCount, setBottomCount] = useState(0);
+  const [catchphrases, setCatchphrases] = useState([]);
   const [currentCatchphrase, setCurrentCatchphrase] = useState('');
   return (
     <div className="main" style={{ backgroundImage: `url(${background})` }}>
@@ -20,15 +24,24 @@ export default function Main() {
         <Controls 
           head={head}
           setHead={setHead}
+          setHeadCount={setHeadCount}
           body={body}
           setBody={setBody}
+          setBodyCount={setBodyCount}
           bottom={bottom}
           setBottom={setBottom}
+          setBottomCount={setBottomCount}
         />
         <Catchphrase
           setCatchphrases={setCatchphrases}
           currentCatchphrase={currentCatchphrase}
           setCurrentCatchphrase={setCurrentCatchphrase}
+        />
+        <Display
+          headCount={headCount}
+          bodyCount={bodyCount}
+          bottomCount={bottomCount}
+          catchphrases={catchphrases}
         />
       </div>
       <Character head={head} body={body} bottom={bottom} />
