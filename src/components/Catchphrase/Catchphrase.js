4f1a11ecbcd576ae95f802a2af23a6e555ca3a3f
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-09-28 19:09:36 -0700

    added catchphrase to catchphrase.js

--- src/components/Catchphrase/Catchphrase.js
+++ src/components/Catchphrase/Catchphrase.js
@@ -1,7 +1,22 @@
-import React from 'react';
 
-export default function Catchphrase() {
+
+export default function Catchphrase({
+  setCatchphrases,
+  currentCatchphrase,
+  setCurrentCatchphrase
+}) {
+  const handleCatchphrase = () => {
+    setCatchphrases((pastCatchphrases) => [...pastCatchphrases, currentCatchphrase]);
+    setCurrentCatchphrase('');
+  };
   return (
-    <div>Catchphrase</div>
+    <div className="catchphrase">
+      <label>Dragonshout:</label>
+      <textarea
+        value={currentCatchphrase}
+        onChange={(e) => setCurrentCatchphrase(e.target.value)}
+      ></textarea>
+      <button onClick={handleCatchphrase}>Use this Dragonshout</button>
+    </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import '../Main/Main.css';
 
 import Controls from '../Controls/Controls';
+import Catchphrase from '../Catchphrase/Catchphrase';
 import Character from '../Character/Character';
 
 import background from '../../background/skyrimBackground.jpg';
@@ -11,6 +12,8 @@ export default function Main() {
   const [head, setHead] = useState('DragonplateHelmet');
   const [body, setBody] = useState('DragonplateArmor');
   const [bottom, setBottom] = useState('DragonplateBoots');
+  const [catchphrase, setCatchphrases] = useState([]);
+  const [currentCatchphrase, setCurrentCatchphrase] = useState('');
   return (
     <div className="main" style={{ backgroundImage: `url(${background})` }}>
       <div className="tools">
@@ -21,6 +24,11 @@ export default function Main() {
           setBody={setBody}
           bottom={bottom}
           setBottom={setBottom}
+        />
+        <Catchphrase
+          setCatchphrases={setCatchphrases}
+          currentCatchphrase={currentCatchphrase}
+          setCurrentCatchphrase={setCurrentCatchphrase}
         />
       </div>
       <Character head={head} body={body} bottom={bottom} />
